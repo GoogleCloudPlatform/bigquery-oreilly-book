f813865f67bce0875d598349eb15a10e95fb713b
Shirley Cohen <scohen@google.com>
2023-03-09 16:32:37 -0600

    added country table

---
+++ 12_datagovernance/create_populate_country.sql
@@ -0,0 +1,41 @@
+create or replace table fitdw.Country(
+	country_code STRING,
+	region STRING
+);
+insert into fitdw.Country(country_code, region) values('us', 'namer');
+insert into fitdw.Country(country_code, region) values('ca', 'namer');
+insert into fitdw.Country(country_code, region) values('mx', 'namer');
+insert into fitdw.Country(country_code, region) values('br', 'latam');
+insert into fitdw.Country(country_code, region) values('ar', 'latam');
+insert into fitdw.Country(country_code, region) values('co', 'latam');
+insert into fitdw.Country(country_code, region) values('pe', 'latam');
+insert into fitdw.Country(country_code, region) values('ec', 'latam');
+insert into fitdw.Country(country_code, region) values('do', 'latam');
+insert into fitdw.Country(country_code, region) values('uk', 'emea');
+insert into fitdw.Country(country_code, region) values('fr', 'emea');
+insert into fitdw.Country(country_code, region) values('it', 'emea');
+insert into fitdw.Country(country_code, region) values('il', 'emea');
+insert into fitdw.Country(country_code, region) values('de', 'emea');
+insert into fitdw.Country(country_code, region) values('pl', 'emea');
+insert into fitdw.Country(country_code, region) values('ua', 'emea');
+insert into fitdw.Country(country_code, region) values('ch', 'emea');
+insert into fitdw.Country(country_code, region) values('nl', 'emea');
+insert into fitdw.Country(country_code, region) values('se', 'emea');
+insert into fitdw.Country(country_code, region) values('dk', 'emea');
+insert into fitdw.Country(country_code, region) values('no', 'emea');
+insert into fitdw.Country(country_code, region) values('be', 'emea');
+insert into fitdw.Country(country_code, region) values('lu', 'emea');
+insert into fitdw.Country(country_code, region) values('at', 'emea');
+insert into fitdw.Country(country_code, region) values('gh', 'emea');
+insert into fitdw.Country(country_code, region) values('za', 'emea');
+insert into fitdw.Country(country_code, region) values('ke', 'emea');
+insert into fitdw.Country(country_code, region) values('bg', 'emea');
+insert into fitdw.Country(country_code, region) values('ro', 'emea');
+insert into fitdw.Country(country_code, region) values('in', 'apac');
+insert into fitdw.Country(country_code, region) values('pk', 'apac');
+insert into fitdw.Country(country_code, region) values('jp', 'apac');
+insert into fitdw.Country(country_code, region) values('hk', 'apac');
+insert into fitdw.Country(country_code, region) values('sg', 'apac');
+insert into fitdw.Country(country_code, region) values('tw', 'apac');
+insert into fitdw.Country(country_code, region) values('au', 'apac');
+insert into fitdw.Country(country_code, region) values('nz', 'apac');
