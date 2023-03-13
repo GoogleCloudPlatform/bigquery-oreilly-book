d54a5726f029d6684632ff7aaf04bc54701cd8be
Shirley Cohen <scohen@google.com>
2023-03-13 10:10:04 -0500

    added license header

--- 12_datagovernance/create_row_access_policy.sql
+++ 12_datagovernance/create_row_access_policy.sql
@@ -1,3 +1,18 @@
+--
+-- Copyright 2023 Google LLC
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+--
 -- Stored procedure that creates a row access policy in BigQuery based on a region and accounts to authorize
 -- The list of accounts must be comma delimited and prefixed with either 'user:', 'group:' or 'serviceAccount:' depending on the type of account. 
 -- Be sure to populate the country table before running the stored proc. You can do that as follows:
